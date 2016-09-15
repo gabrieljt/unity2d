c4a7153ab19d3c11834314310e438ae306e09409
Gabriel Trabasso <gabrieljt@gmail.com>
2016-09-15 16:13:16 -0300

    Procedural dungeon map

--- Assets/Scripts/TileMap.cs
+++ Assets/Scripts/TileMap.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor;
+﻿using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 [CustomEditor(typeof(TileMap))]
@@ -95,17 +96,207 @@ public class TileMap : MonoBehaviour
 		collider2D.enabled = false;
 	}
 
+	#region Build Map
+
 	private void BuildMap()
 	{
 		tiles = new Tile[width, height];
+
+		FillTiles(TileType.Water);
+
+		BuildRooms();
+
+		BuildCorridors();
+
+		BuildWalls();
+	}
+
+	public class Room
+	{
+		public int left;
+		public int top;
+		public int width;
+		public int height;
+
+		public bool isConnected = false;
+
+		public int right
+		{
+			get { return left + width - 1; }
+		}
+
+		public int bottom
+		{
+			get { return top + height - 1; }
+		}
+
+		public int centerX
+		{
+			get { return left + width / 2; }
+		}
+
+		public int centerY
+		{
+			get { return top + height / 2; }
+		}
+
+		public bool CollidesWith(Room other)
+		{
+			return !(left > other.right - 1 || top > other.bottom - 1 || right < other.left + 1 || bottom < other.top + 1);
+		}
+	}
+
+	private List<Room> rooms;
+
+	private void FillTiles(TileType type)
+	{
 		for (int i = 0; i < width; i++)
 		{
 			for (int j = 0; j < height; j++)
 			{
-				tiles[i, j] = i == j ? new Tile(TileType.Water) : new Tile(TileType.Floor);
+				tiles[i, j] = new Tile(type);
 			}
 		}
 	}
+
+	private void BuildRooms()
+	{
+		rooms = new List<Room>();
+
+		int attemptsLeft = 10;
+
+		while (rooms.Count < 10 && attemptsLeft > 0)
+		{
+			int roomWidth = Random.Range(4, 14);
+			int roomHeight = Random.Range(4, 10);
+
+			Room room = new Room();
+			room.left = Random.Range(0, width - roomWidth);
+			room.top = Random.Range(0, height - roomHeight);
+			room.width = roomWidth;
+			room.height = roomHeight;
+
+			if (!RoomCollides(room))
+			{
+				rooms.Add(room);
+			}
+			else
+			{
+				attemptsLeft--;
+			}
+		}
+
+		foreach (Room room in rooms)
+		{
+			BuildRoom(room);
+		}
+	}
+
+	private bool RoomCollides(Room room)
+	{
+		foreach (Room otherRoom in rooms)
+		{
+			if (room.CollidesWith(otherRoom))
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	private void BuildRoom(Room room)
+	{
+		for (int x = 0; x < room.width; x++)
+		{
+			for (int y = 0; y < room.height; y++)
+			{
+				if (x == 0 || x == room.width - 1 || y == 0 || y == room.height - 1)
+				{
+					tiles[room.left + x, room.top + y] = new Tile(TileType.Wall);
+				}
+				else
+				{
+					tiles[room.left + x, room.top + y] = new Tile(TileType.Floor);
+				}
+			}
+		}
+	}
+
+	private void BuildCorridors()
+	{
+		for (int i = 0; i < rooms.Count; i++)
+		{
+			if (!rooms[i].isConnected)
+			{
+				int j = Random.Range(1, rooms.Count);
+				BuildCorridor(rooms[i], rooms[(i + j) % rooms.Count]);
+			}
+		}
+	}
+
+	private void BuildCorridor(Room sourceRoom, Room targetRoom)
+	{
+		int x = sourceRoom.centerX;
+		int y = sourceRoom.centerY;
+
+		while (x != targetRoom.centerX)
+		{
+			tiles[x, y] = new Tile(TileType.Floor);
+
+			x += x < targetRoom.centerX ? 1 : -1;
+		}
+
+		while (y != targetRoom.centerY)
+		{
+			tiles[x, y] = new Tile(TileType.Floor);
+
+			y += y < targetRoom.centerY ? 1 : -1;
+		}
+
+		sourceRoom.isConnected = true;
+		targetRoom.isConnected = true;
+	}
+
+	private void BuildWalls()
+	{
+		for (int x = 0; x < width; x++)
+		{
+			for (int y = 0; y < height; y++)
+			{
+				if (tiles[x, y].Type == TileType.Water && HasAdjacentFloor(x, y))
+				{
+					tiles[x, y] = new Tile(TileType.Wall);
+				}
+			}
+		}
+	}
+
+	private bool HasAdjacentFloor(int x, int y)
+	{
+		if (x > 0 && tiles[x - 1, y].Type == TileType.Floor)
+			return true;
+		if (x < width - 1 && tiles[x + 1, y].Type == TileType.Floor)
+			return true;
+		if (y > 0 && tiles[x, y - 1].Type == TileType.Floor)
+			return true;
+		if (y < height - 1 && tiles[x, y + 1].Type == TileType.Floor)
+			return true;
+
+		if (x > 0 && y > 0 && tiles[x - 1, y - 1].Type == TileType.Floor)
+			return true;
+		if (x < width - 1 && y > 0 && tiles[x + 1, y - 1].Type == TileType.Floor)
+			return true;
+
+		if (x > 0 && y < height - 1 && tiles[x - 1, y + 1].Type == TileType.Floor)
+			return true;
+		if (x < width - 1 && y < height - 1 && tiles[x + 1, y + 1].Type == TileType.Floor)
+			return true;
+
+		return false;
+	}
+
+	#endregion Build Map
 
 	#region Build Texture
 
