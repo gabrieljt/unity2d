e173e77fe474627086fec7be3eb1c0f40d97f790
Gabriel Trabasso <gabrieljt@gmail.com>
2016-09-15 13:42:14 -0300

    TileMapData

--- Assets/Scripts/TileMapData.cs
+++ Assets/Scripts/TileMapData.cs
@@ -1,15 +1,6 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System;
 
-public class TileMapData : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+[Serializable]
+public class TileMapData
+{
 }
