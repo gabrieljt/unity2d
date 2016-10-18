f6434882d1f53f3ca20ca25fa6f30f1baf0e89cc
Gabriel Trabasso <gabrieljt@gmail.com>
2016-10-18 10:13:43 -0200

    CharacterMovement; needs work on state change

--- Assets/Scripts/Actors/Character.cs
+++ Assets/Scripts/Actors/Character.cs
@@ -1,52 +1,28 @@
 ﻿using System;
 using UnityEngine;
 
-public enum CharacterState
-{
-	Idle,
-	Moving,
-	FallingBack,
-}
-
 [RequireComponent(
 	typeof(CharacterMovement),
-	typeof(SpriteRenderer),
-	typeof(Rigidbody2D)
-//typeof(CircleCollider2D)
+	typeof(SpriteRenderer)
 )]
 public class Character : AActor
 {
-	[SerializeField]
-	private CharacterState state = CharacterState.Idle;
-
-	public CharacterState State { get { return state; } }
-
 	[SerializeField]
 	private int steps = 0;
 
 	public int Steps { get { return steps; } }
 
 	[SerializeField]
-	[Range(0.5f, 3f)]
-	private float speed = 3f;
-
 	private SpriteRenderer spriteRenderer;
-
-	private Rigidbody2D rigidbody2D;
-
-	public Action StepTaken = delegate { };
 
 	[SerializeField]
 	private CharacterMovement characterMovement;
 
+	public Action StepTaken = delegate { };
+
 	private void Awake()
 	{
 		spriteRenderer = GetComponent<SpriteRenderer>();
-
-		rigidbody2D = GetComponent<Rigidbody2D>();
-		rigidbody2D.gravityScale = 0f;
-		rigidbody2D.collisionDetectionMode = CollisionDetectionMode2D.Discrete;
-		rigidbody2D.freezeRotation = true;
 
 		characterMovement = GetComponent<CharacterMovement>();
 		characterMovement.DestinationSet += OnDestinationSet;
@@ -67,72 +43,21 @@ public class Character : AActor
 		}
 	}
 
-	private void Start()
-	{
-	}
-
-	private void UpdateMovementState()
-	{
-		/*if (state == CharacterState.FallingBack)
-		{
-			if (ReachedDestination)
-			{
-				Debug.LogWarning("Destination Reached from fallback");
-				return;
-			}
-		}*/
-	}
-
-	/*private void CollisionFallback()
-	{
-		destination = characterMovement.PreviousDestination;
-
-		MovementHalted();
-
-		state = CharacterState.FallingBack;
-
-		Debug.Log("Collided: falling back to " + destination);
-	}*/
-
-	private void OnCollisionEnter2D()
-	{
-		//CollisionFallback();
-	}
-
-	private void OnCollisionStay2D()
-	{
-		if (state != CharacterState.FallingBack)
-		{
-			//CollisionFallback();
-
-			Debug.LogWarning("CollisionStay Fallback");
-		}
-	}
-
 	public override void Enable()
 	{
 		base.Enable();
 		gameObject.SetActive(true);
-		if (Application.isPlaying)
-
-		{
-			rigidbody2D.isKinematic = false;
-		}
 	}
 
 	public override void Disable()
 	{
 		base.Disable();
-
-		if (Application.isPlaying)
-		{
-			rigidbody2D.isKinematic = true;
-		}
-
 		gameObject.SetActive(false);
 	}
 
 	public override void Dispose()
 	{
+		characterMovement.DestinationSet -= OnDestinationSet;
+		characterMovement.DestinationReached -= OnDestinationReached;
 	}
 }
--- Assets/Scripts/Movements/CharacterMovement.cs
+++ Assets/Scripts/Movements/CharacterMovement.cs
@@ -5,42 +5,41 @@ public enum CharacterMovementState
 {
 	Idle,
 	Moving,
-	Collided,
+	FallingBack,
 }
 
 [RequireComponent(
+	typeof(Character),
 	typeof(Rigidbody2D),
 	typeof(CircleCollider2D)
 )]
-public class CharacterMovement : MonoBehaviour
+public class CharacterMovement : MonoBehaviour, IDisposable, IDestroyable
 {
 	[SerializeField]
 	private CharacterMovementState state = CharacterMovementState.Idle;
 
 	[SerializeField]
-	private float speed;
+	[Range(1, 50)]
+	private int speed;
 
 	[SerializeField]
 	private Vector2 previousDestination, destination;
 
 	public Vector2 PreviousDestination { get { return previousDestination; } }
 
+	[SerializeField]
+	private Character character;
 
+	[SerializeField]
 	private Rigidbody2D rigidbody2D;
 
 	public bool ReachedDestination
 	{
 		get
 		{
-			return Vector2.Distance(destination, transform.position) <= 0.05f;
+			return Vector2.Distance(destination, transform.position) <= 0.025f;
 		}
 	}
-
-	public Action<Vector2> DestinationSet = delegate { };
-
-	public Action DestinationReached = delegate { };
-
-	public Action MovementStopped = delegate { };
 
 	private bool CanMove
 	{
@@ -52,21 +51,40 @@ public class CharacterMovement : MonoBehaviour
 
 	public Vector2 Position { get { return rigidbody2D.position; } }
 
+	public Action<Vector2> DestinationSet = delegate { };
+
+	public Action DestinationReached = delegate { };
+
+	public Action MovementStopped = delegate { };
+
+	private Action<MonoBehaviour> destroyed = delegate { };
+
+	public Action<MonoBehaviour> Destroyed { get { return destroyed; } set { destroyed = value; } }
+
 	private void Awake()
 	{
+		character = GetComponent<Character>();
+		character.Enabled += OnCharacterEnabled;
+		character.Disabled += OnCharacterDisabled;
+
 		rigidbody2D = GetComponent<Rigidbody2D>();
 		rigidbody2D.gravityScale = 0f;
 		rigidbody2D.collisionDetectionMode = CollisionDetectionMode2D.Discrete;
 		rigidbody2D.freezeRotation = true;
 	}
 
+	private void Start()
+	{
+		previousDestination = destination = transform.position;
+	}
+
 	private void Update()
 	{
-		if (state == CharacterMovementState.Moving)
+		if (state == CharacterMovementState.Moving || state == CharacterMovementState.FallingBack)
 		{
 			if (ReachedDestination)
 			{
-				StopMoving();
+				SetPosition();
 				DestinationReached();
 				Debug.LogWarning("Destination Reached");
 			}
@@ -77,21 +95,23 @@ public class CharacterMovement : MonoBehaviour
 	{
 		if (CanMove)
 		{
-			SetDestination(direction, out destination);
+			SetDestination(direction, out previousDestination, out destination);
 			DestinationSet(destination);
 		}
 	}
 
-	private void SetDestination(Vector2 direction, out Vector2 destination)
+	private void SetDestination(Vector2 direction, out Vector2 previousDestination, out Vector2 destination)
 	{
 		state = CharacterMovementState.Moving;
+		previousDestination = Position;
 		destination = Position + direction;
 
 		Debug.Log("Moving to " + destination);
 	}
 
-	private void StopMoving()
+	private void SetPosition()
 	{
+		transform.position = destination;
 		MovementStopped();
 
 		state = CharacterMovementState.Idle;
@@ -99,7 +119,7 @@ public class CharacterMovement : MonoBehaviour
 
 	private void FixedUpdate()
 	{
-		if (state == CharacterMovementState.Moving)
+		if (state == CharacterMovementState.Moving || state == CharacterMovementState.FallingBack)
 		{
 			MoveToDestination();
 		}
@@ -109,12 +129,80 @@ public class CharacterMovement : MonoBehaviour
 	{
 		if (!ReachedDestination)
 		{
-			transform.position = Vector3.Lerp(transform.position, new Vector3(destination.x, destination.y, 0f), Mathf.Clamp(speed * 0.1f, 0.05f, 0.25f));
+			transform.position = Vector3.Lerp(transform.position, new Vector3(destination.x, destination.y, 0f), Mathf.Clamp(speed * 0.01f, 0.01f, 0.50f));
 		}
-		else
+	}
+
+	private void FallBack()
+	{
+		state = CharacterMovementState.FallingBack;
+		destination = previousDestination;
+		Debug.Log("Falling back to " + previousDestination);
+	}
+
+	private void OnCollisionEnter2D(Collision2D other)
+	{
+		Collided(other);
+	}
+
+	private void OnCollisionStay2D(Collision2D other)
+	{
+		Collided(other);
+	}
+
+	private void Collided(Collision2D other)
+	{
+		if (state == CharacterMovementState.Idle)
 		{
 			transform.position = destination;
+			return;
 		}
+
+		var otherCharacterMovement = other.gameObject.GetComponent<CharacterMovement>();
+		if (otherCharacterMovement)
+		{
+			if (otherCharacterMovement.state == CharacterMovementState.FallingBack)
+			{
+				return;
+			}
+
+			if (otherCharacterMovement.state == CharacterMovementState.Idle)
+			{
+				FallBack();
+				return;
+			}
+
+			if (otherCharacterMovement.state == CharacterMovementState.Moving)
+			{
+				otherCharacterMovement.FallBack();
+				return;
+			}
+
+			return;
+		}
+
+		FallBack();
 		return;
+	}
+
+	private void OnCharacterEnabled(AActor character)
+	{
+		rigidbody2D.isKinematic = false;
+	}
+
+	private void OnCharacterDisabled(AActor character)
+	{
+		rigidbody2D.isKinematic = true;
+	}
+
+	public void Dispose()
+	{
+		character.Enabled -= OnCharacterEnabled;
+		character.Disabled -= OnCharacterDisabled;
+	}
+
+	public void OnDestroy()
+	{
+		Dispose();
 	}
 }
